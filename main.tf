99e060ae51e2797b06d580ace16ea4fbbc614b50
Filipe Gomes <filipeandradegomes@gmail.com>
2025-05-28 20:00:33 -0300

    refactor: vpc and added igw resource

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
 resource "aws_vpc" "this" {
-  cidr_block           = var.cidr
+  cidr_block           = var.cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
+  tags                 = var.vpc_tags
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
   tags = {
-    Name = "vpc-core"
+    Name = format("%s-igw", var.vpc_tags["Name"])
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,14 @@
-variable "cidr" {
+variable "cidr_block" {
   description = "cidr of vpc"
   type        = string
   default     = "10.0.0.0/24"
 }
+
+variable "vpc_tags" {
+  type = map(string)
+  default = {
+    "Name" = "change-me"
+  }
+}
+
+
