8e9a44cf548bb0053faf15acca2f7d5ca36e0274
Filipe Gomes <filipeandradegomes@gmail.com>
2025-05-28 21:55:09 -0300

    refactor: dynamic region

--- public.tf
+++ public.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "public_subnet_1a" {
   vpc_id            = aws_vpc.this.id
   cidr_block        = cidrsubnet(var.cidr_block, 3, 0)
-  availability_zone = "us-east-1a"
+  availability_zone = "${data.aws_region.current.name}a"
   tags = {
     Name = "devops-subnet-1a"
   }
@@ -11,7 +11,7 @@ resource "aws_subnet" "public_subnet_1a" {
 resource "aws_subnet" "public_subnet_1b" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = cidrsubnet(var.cidr_block, 3, 1)
-  availability_zone       = "us-east-1b"
+  availability_zone       = "${data.aws_region.current.name}b"
   map_public_ip_on_launch = true
   tags = {
     Name = "devops-subnet-1b"
