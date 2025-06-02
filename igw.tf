886f8543159b726042d30447f23c13d86fbc5db8
Filipe Gomes <filipeandradegomes@gmail.com>
2025-06-02 07:38:24 -0300

    feat: adding public route table

--- igw.tf
+++ igw.tf
@@ -6,3 +6,12 @@ resource "aws_internet_gateway" "this" {
   )
 }
 
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+  tags = merge(local.tags, { Name = "devops-public-rtb" })
+}
--- public.tf
+++ public.tf
@@ -13,3 +13,13 @@ resource "aws_subnet" "public_subnet_1b" {
   map_public_ip_on_launch = true
   tags                    = merge(local.tags, { Name = "devops-pub-subnet-1b" })
 }
+
+resource "aws_route_table_association" "public_route_table_association_pub_1a" {
+  subnet_id      = aws_subnet.public_subnet_1a.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_route_table_association_pub_1b" {
+  subnet_id      = aws_subnet.public_subnet_1b.id
+  route_table_id = aws_route_table.public_route_table.id
+}
