ab7c1a46054e63759f236c5a3ecdf3fb3a716cb8
Filipe Gomes <filipeandradegomes@gmail.com>
2025-06-02 08:54:40 -0300

    feat(variables): add enable_dns_support and enable_dns_hostnames

--- variables.tf
+++ variables.tf
@@ -10,4 +10,14 @@ variable "vpc_tags" {
   }
 }
 
+variable "enable_dns_support" {
+  type        = bool
+  description = "Enable DNS support in the VPC"
+  default     = true
+}
 
+variable "enable_dns_hostnames" {
+  type        = bool
+  description = "Enable DNS hostnames in the VPC"
+  default     = true
+}
