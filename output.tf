81c16c338e7ef3b76469eeb8218e9dd09506585f
Filipe Gomes <filipeandradegomes@gmail.com>
2025-06-02 08:36:32 -0300

    feat: output subnets

---
+++ output.tf
@@ -0,0 +1,15 @@
+output "subnet_pub_1a" {
+  value = aws_subnet.public_subnet_1a.id
+}
+
+output "subnet_pub_1b" {
+  value = aws_subnet.public_subnet_1b.id
+}
+
+output "subnet_priv_1a" {
+  value = aws_subnet.private_subnet_1a.id
+}
+
+output "subnet_priv_1b" {
+  value = aws_subnet.private_subnet_1b.id
+}
